652c9c95345a169938cec101c83640ddad2be2aa
emiiryv <111075935+emiiryv@users.noreply.github.com>
2023-12-23 20:07:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        UptodateDoping x = new UptodateDoping(100, new double[]{(10.0),(18.0)});
+        x.calculate();
+
+    }
+}
